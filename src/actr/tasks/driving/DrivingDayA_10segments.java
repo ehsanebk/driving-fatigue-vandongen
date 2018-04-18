3cad59410e51cbcb5edee64e3fac92a1cf472107
Ehsan Khosroshahi <eb452@drexel.edu>
2018-04-18 18:25:15 -0400

    no message

--- src/actr/tasks/driving/DrivingDayA_10segments.java
+++ src/actr/tasks/driving/DrivingDayA_10segments.java
@@ -31,9 +31,9 @@ public class DrivingDayA_10segments extends Task {
 	private final double steerFactor_dfa = (16 * scale);
 	private final double steerFactor_dna = (4 * scale);
 	private final double steerFactor_na = (3 * scale);
-	private final double accelFactor_thw = (2 * .40); // 1 orig, 3?
-	private final double accelFactor_dthw = (4 * .40); // 3 orig, 5?
-	private final double steerNaMax = .01; // orig 0.7
+	private final double accelFactor_thw = (5 * .40); // 1 orig, 3?
+	private final double accelFactor_dthw = (7 * .40); // 3 orig, 5?
+	private final double steerNaMax = .07; // orig 0.7
 	private final double thwFollow = 1.0; // 1.0 orig
 
 	private final double simulationDurarion = 60 * 30; // the driving sessions are 30 min (30 * 60sec)
